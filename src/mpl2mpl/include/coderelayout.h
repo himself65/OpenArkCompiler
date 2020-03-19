c6bd23daad41f5bd25575aa6e9e454e37606d60f
binaryfz <fuzhou@huawei.com>
2020-03-19 11:33:26 +0800

    fix lcn header

--- src/mpl2mpl/include/coderelayout.h
+++ src/mpl2mpl/include/coderelayout.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) [2020] Huawei Technologies Co.,Ltd.All rights reserved.
+ * Copyright (c) [2019-2020] Huawei Technologies Co.,Ltd.All rights reserved.
  *
  * OpenArkCompiler is licensed under the Mulan PSL v1.
  * You can use this software according to the terms and conditions of the Mulan PSL v1.
